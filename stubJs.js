190d553dea365bcbd726f000bc81cb830262626e
mooioom <eldad_levi@hotmail.com>
2015-10-12 16:37:19 +0300

    added static array

--- stubJs.js
+++ stubJs.js
@@ -22,6 +22,16 @@ window.stubJs = function( settings, amount ){
 
 	var data;
 
+	function parseMesulsalim(s){
+		if(s.indexOf('{{') != -1){
+			s = s.replace( /{{\s*([^}]+)\s*}}/g,function(){
+				var exp = arguments[0].match(/\{\{(.*)\}\}/)[1];
+				return stubJs(exp);
+			});
+			return s;
+		}else return s;
+	}
+
 	function parse(s,idx){
 
 		var props = typeof s == 'string' ? s.match(/\((.*)\)/) : null; if(props) props = props[1].split(',');
@@ -33,16 +43,12 @@ window.stubJs = function( settings, amount ){
 		if(s.indexOf('string')  === 0) return getString.apply(this,props);
 		if(s.indexOf('number')  === 0) return getNumber.apply(this,props);
 		if(s.indexOf('boolean') === 0) return getBoolean.apply(this,props);
+		if(s.indexOf('array')   === 0) return getArray.call(this,props,idx);
 		if(s.indexOf('random')  === 0) return getRandom.apply(this,props);
 		if(s.indexOf('color')   === 0) return getColor.apply(this,props);
 		if(s.indexOf('index')   === 0) return idx;
 
-		if(s.indexOf('{{') != -1){
-			s = s.replace( /{{\s*([^}]+)\s*}}/g,function(){
-				var exp = arguments[0].match(/\{\{(.*)\}\}/)[1];
-				return stubJs(exp);
-			})
-		}
+		s = parseMesulsalim(s);
 
 		return s;
 
@@ -65,6 +71,7 @@ window.stubJs = function( settings, amount ){
 	function getRandom(){ if(!arguments.length) return getNumber(); return arguments[Math.floor(Math.random(1)*arguments.length)]; }
 	function getColor(){ return '#'+Math.random().toString(16).substr(-6); }
 	function getData( s, idx ){ var item = {}; for(var x in s) item[x] = parse(s[x],idx); return item; }
+	function getArray( s, idx ){ return parseMesulsalim(s[idx]); }
 
 	if(typeof settings == 'string') {
 		if(typeof amount == 'undefined') return parse(settings);
